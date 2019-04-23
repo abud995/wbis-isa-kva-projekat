e0c3a3c314b7284956727a30b591f35385bf9fde
Dejan Markovic <d.markovic@rocketmail.com>
2019-04-23 17:43:36 +0200

    dodta View i AddressController

---
+++ src/main/java/wis/utils/View.java
@@ -0,0 +1,24 @@
+package wis.utils;
+
+public class View {
+	public interface HideOptionalProperties {};
+	public interface ShowAddress {};
+	public interface ShowCalling {};
+	public interface ShowCallingType {};
+	public interface ShowClassType {};
+	public interface ShowCountry {};
+	public interface ShowCourse {};
+	public interface ShowCourseAttending {};
+	public interface ShowRealization {};
+	public interface ShowTeaching {};
+	public interface ShowFaculty {};
+	public interface ShowPlace {};
+	public interface ShowResult {};
+	public interface ShowScientificField {};
+	public interface ShowStudent {};
+	public interface ShowStudentYear {};
+	public interface ShowStudyProgram {};
+	public interface ShowTeacher {};
+	public interface ShowUniversity {};
+	public interface ShowYearOfStudy {};
+}
